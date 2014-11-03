7f96585ba672c9d5d999e649e3fd3d82111763c4
Michal Sänger <michal.sanger@gmail.com>
2014-11-04 00:12:13 +0100

    Options added

--- Command/PhotosetList.php
+++ Command/PhotosetList.php
@@ -4,6 +4,7 @@ namespace FlickrDownloadr\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -18,13 +19,27 @@ class PhotosetList extends Command
     {
         $this
             ->setName('photoset:list')
-            ->setDescription('List of photosets');
+            ->setDescription('List of photosets')
+            ->addOption(
+                'rows',
+                'r',
+                InputOption::VALUE_OPTIONAL,
+                'Number of photosets returned',
+                20
+            )
+            ->addOption(
+                'all',
+                'a',
+                InputOption::VALUE_NONE,
+                'If set, all photosets will be returned'
+            )
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->flickrApi = $this->getFlickrApi();
-        $sets = $this->getPhotosets();
+        $sets = $this->getPhotosets($input);
         $output->writeln('<info>Number of photosets: ' . count($sets) . '</info>');
         foreach ($sets as $set) {
             $output->writeln($set->attributes()->id . '; ' . $set->title);
@@ -52,11 +67,24 @@ class PhotosetList extends Command
     }
     
     /**
+     * @param InputInterface $input
      * @return \SimpleXMLElement[]
      */
-    private function getPhotosets()
+    private function getPhotosets(InputInterface $input)
     {
-        $xml = $this->flickrApi->call('flickr.photosets.getList');
+        $rows = $input->getOption('rows');
+        if (!is_numeric($rows)) {
+            $rows = 20;
+        }
+        $params = array(
+            'page' => 1,
+            'per_page' => (int)$rows,
+        );
+        if ($input->getOption('all')) {
+            unset($params['per_page']);
+        }
+
+        $xml = $this->flickrApi->call('flickr.photosets.getList', $params);
         $sets = $xml->photosets->photoset;
         return $sets;
     }
