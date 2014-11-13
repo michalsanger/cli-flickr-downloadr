a3d3e1818c11f0cbb1c9d71659a90ef98e93c84c
Michal Sänger <michal.sanger@gmail.com>
2014-11-13 23:06:30 +0100

    Robot loader

--- flickrDownloadr.php
+++ flickrDownloadr.php
@@ -1,30 +1,24 @@
 #!/usr/bin/env php
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/Command/PhotosetDownload.php';
-require_once __DIR__ . '/Command/PhotosetList.php';
-require_once __DIR__ . '/Command/Authorize.php';
-require_once __DIR__ . '/FlickrApi/Client.php';
-require_once __DIR__ . '/FlickrApi/Exception.php';
-require_once __DIR__ . '/FlickrApi/GuzzleJsonAdapter.php';
-require_once __DIR__ . '/Photoset/Photoset.php';
-require_once __DIR__ . '/Photoset/Repository.php';
-require_once __DIR__ . '/Photo/Photo.php';
-require_once __DIR__ . '/Photo/Repository.php';
 
 $configurator = new Nette\Configurator();
 $configurator->setTempDirectory('/tmp');
+
+$robotLoader = $configurator->createRobotLoader();
+$robotLoader->autoRebuild = TRUE;
+$robotLoader->addDirectory('Command');
+$robotLoader->addDirectory('FlickrApi');
+$robotLoader->addDirectory('Photoset');
+$robotLoader->addDirectory('Photo');
+$robotLoader->register();
+
 $configurator->addConfig(__DIR__ . '/config.neon');
 $userConfig = $_SERVER['HOME'] . '/.flickrDownloadr.neon';
 if (is_readable($userConfig)) {
     $configurator->addConfig($userConfig);
 }
 $container = $configurator->createContainer();
-
-//$robotLoader = $configurator->createRobotLoader();
-//$robotLoader->autoRebuild = TRUE;
-//$robotLoader->addDirectory('.');
-//$robotLoader->register();
 
 $application = new Symfony\Component\Console\Application('Flickr Downloadr');
 $commands = $container->findByTag('command');
