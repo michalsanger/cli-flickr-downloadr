1e4cd85ddc66afff5bc2f3384db8e08bacfd0eee
Michal Sänger <michal.sanger@gmail.com>
2014-11-30 01:03:22 +0100

    Setup tests

--- src/flickrDownloadr.php
+++ src/flickrDownloadr.php
@@ -1,17 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-
-require_once __DIR__ . '/Command/PhotosetDownload.php';
-require_once __DIR__ . '/Command/PhotosetList.php';
-require_once __DIR__ . '/Command/Authorize.php';
-require_once __DIR__ . '/FlickrApi/Client.php';
-require_once __DIR__ . '/FlickrApi/Exception.php';
-require_once __DIR__ . '/FlickrApi/GuzzleJsonAdapter.php';
-require_once __DIR__ . '/Photoset/Photoset.php';
-require_once __DIR__ . '/Photoset/Repository.php';
-require_once __DIR__ . '/Photo/Photo.php';
-require_once __DIR__ . '/Photo/Repository.php';
-require_once __DIR__ . '/Oauth/ClientFactory.php';
+require_once __DIR__ . '/bootstrap.php';
 
 $configurator = new Nette\Configurator();
 $configurator->setTempDirectory(sys_get_temp_dir());
---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/bootstrap.php';
+
+date_default_timezone_set('Europe/Prague');
