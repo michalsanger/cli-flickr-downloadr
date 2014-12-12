dd539ded5384746047b4fead320dd85b98e93a4e
Michal Sänger <michal.sanger@gmail.com>
2014-12-12 23:06:32 +0100

    Good enough

--- src/Photo/Downloader.php
+++ src/Photo/Downloader.php
@@ -29,7 +29,46 @@ class Downloader
 			return 0;
 		}
 		\Nette\Utils\FileSystem::createDir($dirname . '/' . dirname($filename));
+		
+		$this->setupProgressBar();
 		$output = $this->output;
+		$progress = null;
+		$streamNotificationCallback = function($notification_code, $severity, $message, $messageCode, $bytesTransferred, $bytesMax) use (&$progress, $output, $filename)
+		{
+			switch($notification_code) {
+
+				case STREAM_NOTIFY_FILE_SIZE_IS:
+					$progress = new \Symfony\Component\Console\Helper\ProgressBar($output, $bytesMax);
+					$progress->setFormat('%message% %final_report%' . "\n" . '%percent:3s%% of %photo_size% [%bar%] %downloaded_bytes% eta %estimated:6s%');
+					$progress->setMessage($filename);
+					$progress->setMessage('', 'final_report');
+					$progress->start();
+
+					break;
+
+				case STREAM_NOTIFY_PROGRESS:
+					$progress->setCurrent($bytesTransferred);
+					break;
+			}
+		};
+		$ctx = stream_context_create();
+		stream_context_set_params($ctx, array("notification" => $streamNotificationCallback));
+
+        $bytes = file_put_contents($dirname . '/' . $filename, fopen($url, 'r', FALSE, $ctx));
+		if ($bytes === FALSE) {
+			$progress->setMessage('<error>Error!</error>', 'final_report');
+		} else {
+			list($time, $size, $speed) = $this->getFinalStats($progress->getMaxSteps(), $progress->getStartTime());
+			$progress->setMessage('<comment>[' . $size . ' in ' . $time . ' (' . $speed . ')]</comment>', 'final_report');
+			$progress->setFormat('%message% %final_report%' . "\n");
+		}
+		$progress->finish();
+		$this->output->writeln('');
+        return $bytes;
+	}
+	
+	private function setupProgressBar()
+	{
 		\Symfony\Component\Console\Helper\ProgressBar::setPlaceholderFormatterDefinition(
 			'photo_size',
 			function (\Symfony\Component\Console\Helper\ProgressBar $bar) {
@@ -42,34 +81,36 @@ class Downloader
 				return \Latte\Runtime\Filters::bytes($bar->getStep());
 			}
 		);
-		
-		$progress = null;
-		$streamNotificationCallback = function($notification_code, $severity, $message, $messageCode, $bytesTransferred, $bytesMax) use (&$progress, $output, $filename)
-		{
-			switch($notification_code) {
-
-				case STREAM_NOTIFY_FILE_SIZE_IS:
-					$progress = new \Symfony\Component\Console\Helper\ProgressBar($output, $bytesMax);
-					$progress->setFormat('%message% %final_report%' . "\n" . '%percent:3s%% of %photo_size% [%bar%] %downloaded_bytes% eta %estimated:6s%');
-					$progress->setMessage($filename);
-					$progress->setMessage('', 'final_report');
-					$progress->start();
-					break;
-
-				case STREAM_NOTIFY_PROGRESS:
-					$progress->setCurrent($bytesTransferred);
-					break;
-			}
-		};
-		$ctx = stream_context_create();
-		stream_context_set_params($ctx, array("notification" => $streamNotificationCallback));
-
-        $bytes = file_put_contents($dirname . '/' . $filename, fopen($url, 'r', FALSE, $ctx));
-		$photoSize = \Latte\Runtime\Filters::bytes($progress->getMaxSteps());
-		$progress->setMessage('<comment>[' . $photoSize . ' in ?s (??kB/s)]</comment>', 'final_report');
-		$progress->setFormat('%message% %final_report%' . "\n");
-		$progress->finish();
-		$this->output->writeln('');
-        return $bytes;
 	}
+		
+
+
+	/**
+	 * @param int $photoSize
+	 * @param int $startTime
+	 * @return array
+	 */
+	private function getFinalStats($photoSize, $startTime)
+	{
+		$size = \Latte\Runtime\Filters::bytes($photoSize);
+		$seconds = max(array(time() - $startTime, 1));
+		$minutes = min(array(59, floor($seconds/60)));
+		$hours = floor($seconds/3600);
+		
+		$times = array();
+		if ($hours > 0) {
+			$times[] = $hours . 'h';
+		}
+		if ($minutes > 0) {
+			$times[] = $minutes . 'm';
+		}
+		if (($seconds%60) > 0) {
+			$times[] = $seconds%60 . 's';
+		}
+		$time = implode(' ', $times);
+
+		$speed = \Latte\Runtime\Filters::bytes($photoSize / $seconds) . '/s';
+		return array($time, $size, $speed);
+	}
+		
 }
