a6a72f64d97a4abd44ec81e36f4f6fd59ba307b4
Michal Sänger <michal.sanger@gmail.com>
2014-12-20 18:43:32 +0100

    Bump version in config and readme

---
+++ tools/bump-version/bump-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ] 
+  then
+    echo "Version not supplied"
+    exit 1
+fi
+
+sed -i.bak "s/version: \".*\"/version: \"$1\"/g" ../../src/config.neon
+rm ../../src/config.neon.bak
+
+sed -i.bak "s/download\/.*\/flickr_downloadr\.phar/download\/$1\/flickr_downloadr\.phar/g" ../../README.md
+rm ../../README.md.bak
+
+# TODO: commit and create tag
