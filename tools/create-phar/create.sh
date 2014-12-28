749ad4ba715f15165b1465bafa17a837cd023a2b
Michal Sänger <michal.sanger@gmail.com>
2014-12-28 23:42:38 +0100

    Set current git hash as build name

--- tools/create-phar/create.sh
+++ tools/create-phar/create.sh
@@ -3,4 +3,13 @@
 # Install Box2 and put it in the path
 # https://github.com/box-project/box2
 
+# Set current hash as build name into config
+BUILD=`git rev-parse HEAD`
+sed -i.bak "s/build: null/build: \"$BUILD\"/g" ../../src/config.neon
+rm ../../src/config.neon.bak
+
+# Build phar file
 box build
+
+# Revert changes in config
+git checkout -- ../../src/config.neon
